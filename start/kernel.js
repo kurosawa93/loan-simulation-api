3289cefefd33e0a6a15b25e6ae48abc6987c8ab9
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2020-01-19 11:49:47 +0700

    install cors package

--- start/kernel.js
+++ start/kernel.js
@@ -38,4 +38,4 @@ const namedMiddleware = {}
 Server
   .registerGlobal(globalMiddleware)
   .registerNamed(namedMiddleware)
-  .use(['Adonis/Middleware/Static'])
+  .use(['Adonis/Middleware/Static', 'Adonis/Middleware/Cors'])
